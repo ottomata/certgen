a373f0cc34964ce97604819b887b90a03063fdb0
Andrew Otto <acotto@gmail.com>
2017-06-19 10:30:13 -0400

    mv lib/ certgen/, add __init__.py

---
+++ certgen/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+TODO: docs here
+"""
+
+from .ca import *
+from .certificate import *
+from .config import *
+from .key import *
+from .util import *
--- setup.py
+++ setup.py
@@ -2,24 +2,22 @@
 from setuptools import setup, find_packages
 
 try:
-    long_description = open("README.md").read()
+    long_description = open('README.md').read()
 except IOError:
-    long_description = ""
+    long_description = ''
 
 setup(
-    name="certgen",
-    version="0.1.0",
-    description="OpenSSL Certificate Manager",
-    license="Apache",
-    author="Andrew Otto",
+    name='certgen',
+    version='0.1.0',
+    description='OpenSSL Certificate Manager',
+    license='Apache',
+    author='Andrew Otto',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[], # TODO: pyyaml, yamlreader
     long_description=long_description,
+    scripts=(),
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.3",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3',
     ]
 )
-
-
-# pyyaml
